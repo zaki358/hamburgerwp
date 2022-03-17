578ba83861aed4af4d937a17f649b246bbee7f82
zaki358 <t.yamazaki.1129@gmail.com>
2022-03-17 20:40:38 +0900

    画像パス修正

--- functions.php
+++ functions.php
@@ -13,6 +13,11 @@ add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     //add_theme_support('menus');
     add_theme_support('post-thumbnails');
+    register_nav_menus(
+        array(
+            'side-menu' => 'サイドメニュー'
+        )
+    );
 });
 
 //タイトル出力
@@ -30,8 +35,8 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('add_google_fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap ', false);
     wp_enqueue_style('style-css', get_theme_file_uri() . '/css/style.css', array());
     wp_deregister_script('jquery');
-    wp_enqueue_script('jquery', get_theme_file_uri() . 'js/jquery-3.6.0.min.js', array(), '3.6.0', true);
-    wp_enqueue_script('js', get_theme_file_uri() . 'js/script.js', array('jquery'), true);
+    wp_enqueue_script('jquery', get_theme_file_uri() . '/js/jquery-3.6.0.min.js', array(), '3.6.0', true);
+    wp_enqueue_script('js', get_theme_file_uri() . '/js/script.js', array('jquery'), true);
 });
 
 //ウィジェット
@@ -41,20 +46,10 @@ add_action('widgets_init', function () {
             'name' => 'メニュー',
             'id' => 'menu_widget',
             'description' => 'メニュー用ウィジェットです',
-            'before_widget' => '<div id="%1$s" class="p-menu__list__item c-title--menu %2$s">',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget' => '</div>',
-            'before_title' => '<h2 class="p-menu__list__item c-title--menu">',
+            'before_title' => '<h2><i class="fa fa-folder" aria-hidden="true"></i>',
             'after_title' => '</h2>\n'
-        )
-    );
-});
-
-//ナビゲーション
-add_action('after_setup_theme', function () {
-    register_nav_menus(
-        array(
-            'main-menu' => 'ハンバーガーメニュー',
-            'side-menu' => 'サイドメニュー'
         )
     );
 });
