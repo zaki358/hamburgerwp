f8dbf7027535962c95cc7b0b8d3e5efd16f31231
zaki358 <t.yamazaki.1129@gmail.com>
2022-03-18 20:10:39 +0900

    single作成完了

--- functions.php
+++ functions.php
@@ -22,11 +22,7 @@ add_action('after_setup_theme', function () {
 
 //タイトル出力
 add_filter('pre_get_document_title', function ($title) {
-    if (is_front_page() && is_home()) {
         $title = get_bloginfo('name', 'display');
-    } elseif (is_singular()) {
-        $title = single_post_title(",false");
-    }
     return $title;
 });
 
--- single.php
+++ single.php
@@ -10,23 +10,23 @@
             <!--投稿IDとクラスを取得-->
             <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-                <?php the_post_thumbnail(); ?>
-                <div class="p-box--single">
+            <!--バックグランドにするため使用しないthe_post_thumbnail();-->
+            <!-- if文成立不可のため使用しないif (has_post_thumbnail()){-->
+                <div class="p-box--single" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>)">
                     <h1 class="c-title--box"><?php the_title(); ?></h1>
                 </div>
+
                 <div class="u-margin--article">
                     <article class="p-article">
                         <?php the_content(); ?>
                     </article>
                 </div>
-
             </div>
 
         <?php endwhile;
-    else :
-        ?><p>表示する記事がありません</p><?php
-    endif; ?>
-
+        else :?>
+        <p>表示する記事がありません</p><?php
+        endif; ?>
 </main>
 
 <?php get_footer(); ?>
