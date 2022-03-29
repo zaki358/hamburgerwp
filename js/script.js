536062f7350926012678b7857a8df280099c9b17
zaki358 <t.yamazaki.1129@gmail.com>
2022-03-29 23:53:30 +0900

    jsマジックナンバー廃止

--- js/script.js
+++ js/script.js
@@ -1,39 +1,37 @@
 jQuery(function ($) {
+	//Menuボタンをクリックで各要素にis-openクラスを付与
 	$(".js-menu--open").on("click", function () {
-		//$( this ).toggleClass( "is-open" );
 		$(".p-menu").addClass("is-open");
 		$("body").addClass("is-open");
 		$(".c-layer--sidebar").addClass("is-open");
-		//$(".c-btn--menu").addClass("is-open");
-		//$(".c-btn--menu").show();
-
 	});
+
+	//クローズボタン（×ボタン）をクリックでis-openクラスを削除
 	$(".js-menu--close").on("click", function () {
 		$(".p-menu").removeClass("is-open");
 		$("body").removeClass("is-open");
 		$(".c-layer--sidebar").removeClass("is-open");
-		//$(".c-btn--menu").removeClass("is-open");
-		//$(".c-btn--menu").hide();
 	});
 
+	//フロントページのマップ、文字数に合わせて高か変更
+	//コメント、タイトル、線の部分の高さを取得して変数に置き換え
 	$(window).on('load resize', function () {
 		let commentHeight = $(".p-map__comment").outerHeight();
 		let titleHeight = $(".c-title--map").outerHeight();
 		let borderHeight = $(".c-title--map__border--white").outerHeight();
 		let allHeight = commentHeight + titleHeight + borderHeight;
 		if (window.matchMedia('(min-width:1025px)').matches) {
-			allHeight = 165 + allHeight;
+			let margin = 165;
+			allHeight = margin + allHeight;
 			$(".p-map").css('height', allHeight);
 		} else if (window.matchMedia('(min-width:600px)').matches) {
-			allHeight = 80 + allHeight;
+			let margin = 80;
+			allHeight = margin + allHeight;
 			$(".p-map").css('height', allHeight);
 		} else {
-			allHeight = 80 + allHeight;
+			let margin = 80;
+			allHeight = margin + allHeight;
 			$(".p-map").css('height', allHeight);
 		}
-		console.log(commentHeight);
-		console.log(titleHeight);
-		console.log(borderHeight);
-		console.log(allHeight);
 	});
 });
